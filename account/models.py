0d22673cff50616003a218d0649ed21d8e03ef67
Daniel Ntege <danientege785@gmail.com>
2024-03-07 14:59:36 +0300

    commit

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    # Add any other fields you need for authentication and profile information
+
+    def __str__(self):
+        return self.username
--- app/models.py
+++ app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Department(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    
 class Product(models.Model):
     name = models.CharField(max_length=100)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
