7c1fa733d7ea077647792adaa141153e293afb2b
Daniel Ntege <danientege785@gmail.com>
2024-03-08 08:30:53 +0300

    commit

--- account/models.py
+++ account/models.py
@@ -1,6 +1,33 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.contrib.auth.hashers import make_password
 # from app.models import Department
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = CustomUser(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("user_type", 1)
+        extra_fields.setdefault("last_name", "System")
+        extra_fields.setdefault("first_name", "Administrator")
+
+        assert extra_fields["is_staff"]
+        assert extra_fields["is_superuser"]
+        return self._create_user(email, password, **extra_fields)
+
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
--- rms/settings.py
+++ rms/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'administrator',
-    'account',
+    
+    # My Created Applications
+    'app.apps.AppConfig',
+    'administrator.apps.AdministratorConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
