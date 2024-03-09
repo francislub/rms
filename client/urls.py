86d3cb48c01eddd893e0a3eab991ede6f24e8506
Daniel Ntege <danientege785@gmail.com>
2024-03-09 14:12:56 +0300

    commit

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
     path('dashboard/',views.dashboard,name='dashboard'),
 ]
