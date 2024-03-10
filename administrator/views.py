cba6c88edbc680bbfb8b0efc058f18d92f9b5316
Daniel Ntege <danientege785@gmail.com>
2024-03-10 21:11:06 +0300

    commit

--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+# from client.models import Voter, Position, Candidate, Votes
+from account.models import CustomUser
+from account.forms import CustomUserForm
+# from client.forms import *
+from django.contrib import messages
+from django.http import JsonResponse, HttpResponse
+from django.conf import settings
 
-# Create your views here.
+def dashboard(request):
+    return render(request, "client/dashboard.html")
