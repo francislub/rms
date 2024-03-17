5a376a6d8cb18b23d573b3caebb23274da492646
Daniel Ntege <danientege785@gmail.com>
2024-03-17 15:08:57 +0300

    commit

--- client/views.py
+++ client/views.py
@@ -20,8 +20,9 @@ def req(request):
 
 
 #===========Francis calling the dashbourd==============
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def staffDashboard(request):
+    return render(request, 'client/staffDash.html')
+
 def client(request):
     return render(request, 'client.html')
 
@@ -36,14 +37,14 @@ def requisition_phase1(request):
             return redirect('adminDashboard')
     else:
         form = RequisitionPhase1Form()
-    return render(request, 'requisition_phase1.html', {'form': form})
+    return render(request, 'client/req.html', {'form': form})
 
 def requisition_phase2(request):
     if request.method == 'POST':
         form = RequisitionPhase2Form(request.POST)
         if form.is_valid():
             requisition = form.save(commit=False)
-            requisition.phase = 1
+            requisition.phase = 2
             requisition.save()
             return redirect('requisition_phase2')
     else:
@@ -55,9 +56,9 @@ def requisition_phase3(request):
         form = RequisitionPhase3Form(request.POST)
         if form.is_valid():
             requisition = form.save(commit=False)
-            requisition.phase = 1
+            requisition.phase = 3
             requisition.save()
             return redirect('requisition_phase3')
     else:
-        form = RequisitionPhase2Form()
-    return render(request, 'requisition_phase1.html', {'form': form})
+        form = RequisitionPhase3Form()
+    return render(request, 'requisition_phase3.html', {'form': form})
