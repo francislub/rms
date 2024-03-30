3ca60bd3fdd2d4a25ff34dc32a54e7cf27085959
Daniel Ntege <danientege785@gmail.com>
2024-03-30 12:36:03 +0300

    commit

--- staff/models.py
+++ staff/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from client.models import Requisition  # Assuming Requisition model is defined in the client app
-
-User = get_user_model()
+from account.models import CustomUser  # Assuming CustomUser model is defined in the account app
 
 class DepartmentApproval(models.Model):
     requisition = models.ForeignKey(Requisition, on_delete=models.CASCADE)
     department = models.ForeignKey('account.Department', on_delete=models.CASCADE)
-    approved_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    approver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='approver')
+    approved_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    approver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='approver')
     approved_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
