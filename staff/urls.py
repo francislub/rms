e3882b2636549ca742460bc3ec35c44ab177511e
Daniel Ntege <danientege785@gmail.com>
2024-03-30 15:54:32 +0300

    commit

---
+++ staff/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.dashboard, name="adminDashboard"),
+    path('reqst/', views.reqStatus, name="reqst"),
+    # path('repst/', views.reportStatus, name="repst"),
+]
